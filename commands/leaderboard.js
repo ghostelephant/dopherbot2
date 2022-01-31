fa1d1a6e4492e0d461e74bddd98401eaa4408ee1
ghostelephant <gumpman155_2@yahoo.com>
2022-01-31 11:51:44 -0600

    Leaderboard improvements

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,4 +1,4 @@
-const {rightAlign} = require("../utils");
+const {rightAlign, getSessionScores} = require("../utils");
 
 const avgDiffToString = avgdiff => {
   avgdiff = Math.floor(10 * avgdiff);
@@ -8,57 +8,34 @@ const avgDiffToString = avgdiff => {
   return "" + avgdiff;
 };
 
-const getSessionScores = session => {
-  scores = {};
-  for(let round of session){
-    for(let playerId in round){
-      if(playerId === "pitch") continue;
-      if(!(playerId in scores)){
-        scores[playerId] = {
-          points: 0,
-          guesses: 0,
-          diffSum: 0
-        };
-      }
-      scores[playerId].points += round[playerId].score;
-      scores[playerId].guesses++;
-      scores[playerId].diffSum += round[playerId].diff;
+const filterCriteria = args => {
+  const criteriaOptions = [
+    "points", "-points",
+    "avgdiff", "-avgdiff",
+    "guesses", "-guesses"
+  ];
+  const criteria = [];
+  for(let i=args.length - 1; i>=0; i--){
+    const criterion = args[i].toLowerCase();
+    if(!criteriaOptions.includes(criterion)){
+      continue;
     }
+    criteria.push(criterion);
   }
-  for(let playerId in scores){
-    scores[playerId].avgdiff = (scores[playerId].points / scores[playerId].guesses);
-  }
-  return scores;
+  return criteria.length ? criteria : ["avgdiff", "points"];
 };
-
-const reverseArray = arr => {
-  for(let i=0; i<arr.length / 2; i++){
-    [arr[i], arr[arr.length - i - 1]] = [arr[arr.length - i - 1], arr[i]];
-  }
-  return arr;
-}
 
 const sortScores = (scores, criteria) => {
   try {
-    criteria = reverseArray(criteria);
     const scoresArray = Object.keys(scores).map(playerId => {
       return {playerId, ...scores[playerId]};
     });
-    const criteriaOptions = [
-      "points", "-points",
-      "avgdiff", "-avgdiff",
-      "guesses", "-guesses"
-    ];
     const defaultDescending = {
       points: true,
       guesses: true,
       avgdiff: false
     }
     for(let criterion of criteria){
-      criterion = criterion.toLowerCase();
-      if(!criteriaOptions.includes(criterion)){
-        continue;
-      }
       let reverse = false;
       if(criterion[0] === "-"){
         reverse = true;
@@ -84,7 +61,7 @@ const sortScores = (scores, criteria) => {
   }
 };
 
-const generateTable2 = (scoresArray, nicknames, final) => {
+const generateTable = (scoresArray, nicknames, final) => {
   let table = "```\n";
   table += (final ? "FINAL SESSION STANDINGS:\n\n" : "LEADERBOARD:\n\n");
   table += "PLAYER       ║ PTS ║ #Gs ║ AVGDIFF\n";
@@ -104,9 +81,9 @@ const leaderboard = async({msg, guildInfo, client, args}, final = false) => {
       return console.log("Leaderboard will not work until a session has been started.");
     }
     const scores = getSessionScores(guildInfo.currentSession);
-    const criteria = args.length ? args : ["points", "avgdiff"];
+    const criteria = filterCriteria(args);
     const sortedScoresArray = sortScores(scores, criteria);
-    const table = generateTable2(sortedScoresArray, guildInfo.utils.playerNicknames, final);
+    const table = generateTable(sortedScoresArray, guildInfo.utils.playerNicknames, final);
     const channel = client.channels.cache.get(msg.channelId);
     channel.send(table)
       .catch(e => console.log(e));
---
+++ utils/getSessionScores.js
@@ -0,0 +1,24 @@
+const getSessionScores = session => {
+  scores = {};
+  for(let round of session){
+    for(let playerId in round){
+      if(playerId === "pitch") continue;
+      if(!(playerId in scores)){
+        scores[playerId] = {
+          points: 0,
+          guesses: 0,
+          diffSum: 0
+        };
+      }
+      scores[playerId].points += round[playerId].score;
+      scores[playerId].guesses++;
+      scores[playerId].diffSum += round[playerId].diff;
+    }
+  }
+  for(let playerId in scores){
+    scores[playerId].avgdiff = (scores[playerId].points / scores[playerId].guesses);
+  }
+  return scores;
+};
+
+module.exports = getSessionScores;
